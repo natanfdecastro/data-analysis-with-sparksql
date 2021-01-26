98ba3885744e383ffabc2960fd91c31158c364a7
Joshua Philippe Molina Van Den Bergh <72298406+Nostarck@users.noreply.github.com>
2021-01-25 20:33:55 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import unicodedata
 import sys
 import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import trim
 from pyspark.sql.functions import regexp_replace
+from pyspark.sql.functions import translate, regexp_replace
 import pyspark.sql.functions as f
 from pyspark import SQLContext
 
@@ -15,10 +17,9 @@ sql = SQLContext(sc)
 ###
 def initialize_frames():
     oij_data_frame = (sql.read.format("com.databricks.spark.csv").option("header", "true").load(oij_file_name))
-    oij_data_frame.createOrReplaceTempView("schedule")
 
     inec_data_frame = (sql.read.format("com.databricks.spark.csv").option("header", "true").load(inec_file_name))
-    inec_data_frame.createOrReplaceTempView("schedule")
+
     return oij_data_frame,inec_data_frame
 
 def remove_white_space(oij_data_frame,inec_data_frame):
@@ -32,6 +33,7 @@ def remove_white_space(oij_data_frame,inec_data_frame):
 
 def to_lowercase(oij_data_frame, inec_data_frame):
     oij_data_frame = oij_data_frame.withColumn("Distrito",f.lower(f.col("Distrito")))
+
     inec_data_frame = inec_data_frame.withColumn("provincia_canton_distrito",f.lower(f.col("provincia_canton_distrito")))
 
     return oij_data_frame,inec_data_frame
@@ -40,7 +42,61 @@ def show_data_frames(oij_data_frame,inec_data_frame):
     oij_data_frame.show()
     inec_data_frame.show()
 
+
+def make_diff_list(oij_data_frame,inec_data_frame):
+    oij_data_frame.createOrReplaceTempView("OIJ")
+    inec_data_frame.createOrReplaceTempView("INEC")
+    diff_list = sql.sql(
+        "Select OIJ.Distrito from OIJ LEFT JOIN INEC ON OIJ.distrito = INEC.provincia_canton_distrito WHERE INEC.provincia_canton_distrito is NULL GROUP BY OIJ.Distrito");
+    return diff_list
+
+def show_diff_list(oij_data_frame,inec_data_frame):
+    make_diff_list(oij_data_frame,inec_data_frame).show()
+
+def show_how_many_are_diff(oij_data_frame,inec_data_frame):
+    diff_list = make_diff_list(oij_data_frame,inec_data_frame)
+    diff_list.createOrReplaceTempView("diff_table")
+    diff_list = sql.sql("Select count(*) as cantidad from diff_table")
+    diff_list.show();
+
+
+def make_trans():
+    matching_string = ""
+    replace_string = ""
+
+    for i in range(ord(" "), sys.maxunicode):
+        name = unicodedata.name(chr(i), "")
+        if "WITH" in name:
+            try:
+                base = unicodedata.lookup(name.split(" WITH")[0])
+                matching_string += chr(i)
+                replace_string += base
+            except KeyError:
+                pass
+
+    return matching_string, replace_string
+
+def clean_text(c):
+    matching_string, replace_string = make_trans()
+    return translate(
+        regexp_replace(c, "\p{M}", ""),
+        matching_string, replace_string
+    ).alias(c)
+
+def remove_accents(inec_data_frame):
+    return inec_data_frame.select(clean_text("provincia_canton_distrito"))
+
+
 oij_data_frame,inec_data_frame = initialize_frames()
 oij_data_frame,inec_data_frame = remove_white_space(oij_data_frame,inec_data_frame)
 oij_data_frame,inec_data_frame = to_lowercase(oij_data_frame,inec_data_frame)
-show_data_frames(oij_data_frame,inec_data_frame)
+
+#show_diff_list(oij_data_frame,inec_data_frame)
+
+inec_data_frame = remove_accents(inec_data_frame)
+inec_data_frame.show()
+
+#show_diff_list(oij_data_frame,inec_data_frame)
+
+lista = make_diff_list(oij_data_frame,inec_data_frame);
+
